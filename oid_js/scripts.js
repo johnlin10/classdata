52ad1481b545209fed7b979d454ae6e6a07d01bf
Johnolin10 <101758259+johnlin10@users.noreply.github.com>
2022-11-28 11:52:23 +0800

    Add files via upload

---
+++ oid_js/scripts.js
@@ -0,0 +1,40 @@
+/*!
+* Start Bootstrap - Clean Blog v6.0.8 (https://startbootstrap.com/theme/clean-blog)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
+*/
+
+var yes = confirm('此網站已經停止使用，新版班級資料平台已發布！\n！！！按下確定後，請允許彈出式視窗！！！');
+
+if (yes) {
+    window.open('https://johnlin10.github.io/classdata/4.5/classdata.html')
+}
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function() {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if ( currentTop < scrollPos) {
+            // Scrolling Up
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                console.log(123);
+                mainNav.classList.remove('is-visible', 'is-fixed');
+            }
+        } else {
+            // Scrolling Down
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
+    });
+})
+
+setTimeout(() => {
+    $(".loading-div").hide(duration=400);
+    $('body').css('overflow-y','scroll');
+},700);
