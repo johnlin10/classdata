4d1724a291784c17f452cb710b0463e466d30d0d
Johnolin10 <101758259+johnlin10@users.noreply.github.com>
2022-12-11 23:02:12 +0800

    Add files via upload

--- js/FastGo.js
+++ js/FastGo.js
@@ -1,19 +1,29 @@
 var button = document.getElementById("button");
 var card = document.getElementById("card");
 var cardafter = document.getElementById("cardafter")
-var close = document.getElementById("close");
+// var close = document.getElementById("close");
 
 button.addEventListener("click", function() {
     card.classList.add("open");
+    // close.classList.add("close-in")
     cardafter.classList.add("cardafter")
     button.classList.remove("FastGo")
     button.classList.add("FastGo-hide")
 });
 
-close.addEventListener("click", function() {
+// close.addEventListener("click", function() {
+//     card.classList.remove("open");
+//     cardafter.classList.remove("cardafter")
+//     close.classList.remove("close-in")
+//     button.classList.add("FastGo")
+//     button.classList.remove("FastGo-hide")
+// });
+
+cardafter.addEventListener("click", function() {
+    button.classList.remove("FastGo-hide");
+    button.classList.add("FastGo");
+    cardafter.classList.remove("cardafter");
     card.classList.remove("open");
-    cardafter.classList.remove("cardafter")
-    button.classList.add("FastGo")
-    button.classList.remove("FastGo-hide")
+    close.classList.remove("close-in");
 });
 
