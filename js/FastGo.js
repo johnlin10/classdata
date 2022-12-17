e8ff100d59e793d5ea9891ab239201dfa724ea09
Johnolin10 <101758259+johnlin10@users.noreply.github.com>
2022-12-17 20:20:21 +0800

    Add files via upload

--- js/FastGo.js
+++ js/FastGo.js
@@ -1,29 +1,17 @@
 var button = document.getElementById("button");
 var card = document.getElementById("card");
-var cardafter = document.getElementById("cardafter")
-// var close = document.getElementById("close");
+var cardafter = document.getElementById("cardafter");
 
 button.addEventListener("click", function() {
-    card.classList.add("open");
-    // close.classList.add("close-in")
+    card.classList.add("open")
     cardafter.classList.add("cardafter")
     button.classList.remove("FastGo")
     button.classList.add("FastGo-hide")
 });
-
-// close.addEventListener("click", function() {
-//     card.classList.remove("open");
-//     cardafter.classList.remove("cardafter")
-//     close.classList.remove("close-in")
-//     button.classList.add("FastGo")
-//     button.classList.remove("FastGo-hide")
-// });
 
 cardafter.addEventListener("click", function() {
     button.classList.remove("FastGo-hide");
     button.classList.add("FastGo");
     cardafter.classList.remove("cardafter");
     card.classList.remove("open");
-    close.classList.remove("close-in");
 });
-
